e9f7dc125b9bce143f815e649b134e417ac338c8
Samuell <Samuell1@users.noreply.github.com>
2017-09-13 11:47:19 +0200

    Fix getJSON

--- middleware/check-auth.js
+++ middleware/check-auth.js
@@ -2,6 +2,6 @@ import Cookie from 'js-cookie'
 import { getUserFromCookie } from '@/utils/helpers'
 
 export default function ({ store, isServer, req }) {
-  const loggedUser = isServer ? getUserFromCookie(req) : Cookie.get('user')
+  const loggedUser = isServer ? getUserFromCookie(req) : Cookie.getJSON('user')
   if (loggedUser) store.commit('auth/setUser', loggedUser)
 }
