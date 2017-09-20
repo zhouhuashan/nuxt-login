5c501401c3f523082b89a1ad5263f1f50c472da9
Samuell <samuell.patro@gmail.com>
2017-09-20 23:28:12 +0200

    Fix export

--- middleware/check-auth.js
+++ middleware/check-auth.js
@@ -1,5 +1,5 @@
 import Cookie from 'js-cookie'
-import { getUserFromCookie } from '@/utils/helpers'
+import { getUserFromCookie } from '~/utils/helpers'
 
 export default function ({ store, isServer, req }) {
   const loggedUser = isServer ? getUserFromCookie(req) : Cookie.get('user')
--- utils/helpers.js
+++ utils/helpers.js
@@ -1,9 +1,7 @@
-export default {
-  getUserFromCookie (req) {
-    if (!req.headers.cookie) return
-    const cookie = req.headers.cookie.split(';').find(c => c.trim().startsWith('user='))
-    if (!cookie) return
-    const user = decodeURIComponent(cookie.split('=')[1])
-    return JSON.parse(user)
-  }
+export const getUserFromCookie = (req) => {
+  if (!req.headers.cookie) return
+  const cookie = req.headers.cookie.split(';').find(c => c.trim().startsWith('user='))
+  if (!cookie) return
+  const user = decodeURIComponent(cookie.split('=')[1])
+  return JSON.parse(user)
 }
